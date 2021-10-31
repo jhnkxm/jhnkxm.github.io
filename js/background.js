5d0e8a912be47013d33f8a3cd15cd190cf7a5e75
jhnkxm <91812180+jhnkxm@users.noreply.github.com>
2021-11-01 00:54:50 +0900

    Add files via upload

---
+++ js/background.js
@@ -0,0 +1,8 @@
+const images = ["0.jpg", "1.jpg", "3.png", "4.png", "5.jpg"];
+
+const chosenImage = images[Math.floor(Math.random() * images.length)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+bgImage.id = "bgImg";
+document.body.background = "img/" + chosenImage;
